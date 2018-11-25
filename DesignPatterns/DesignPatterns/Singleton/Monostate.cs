1bfb13a498f4c990159fbd340c48330d49b79231
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-11-25 15:30:37 +0100

    Monostate

---
+++ DesignPatterns/DesignPatterns/Singleton/Monostate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    public class Monostate
+    {
+        public void Demo()
+        {
+            var ceo1 = new CEO {Name = "Adam", Age = 55};
+            
+            var ceo2 = new CEO {Age = 50};
+
+            Console.WriteLine($"{nameof(ceo1)} {ceo1}");
+            Console.WriteLine($"{nameof(ceo2)} {ceo2}");
+        }
+    }
+
+    public class CEO
+    {
+        private static string _name;
+
+        private static int _age;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+}
