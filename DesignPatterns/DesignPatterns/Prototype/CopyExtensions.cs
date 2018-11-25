a8cb78b1020930a53aa499f7ade1b21f71d4b4b6
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-11-25 15:30:28 +0100

    Singleton

--- DesignPatterns/DesignPatterns/Prototype/CopyExtensions.cs
+++ DesignPatterns/DesignPatterns/Prototype/CopyExtensions.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 using System.Xml.Serialization;
 
 namespace DesignPatterns.Prototype
---
+++ DesignPatterns/DesignPatterns/Singleton/Database.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using MoreLinq;
+using System.Reflection.Metadata.Ecma335;
+using System.Linq;
+
+namespace DesignPatterns.Singleton
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+
+    public class Database : IDatabase
+    {
+        private readonly Dictionary<string, int> _capitols;
+
+        private Database()
+        {
+            _capitols = File.ReadAllLines(
+                    Path.Combine(
+                        new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName, "Singleton/capitals.txt")
+                )
+                .Batch(2)
+                .ToDictionary(
+                    list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1)));
+        }
+
+        private static readonly Lazy<Database> _instance = new Lazy<Database>(() => new Database());
+
+        public static Database Instance = _instance.Value;
+
+        public int GetPopulation(string name) => _capitols[name];
+    }
+}
