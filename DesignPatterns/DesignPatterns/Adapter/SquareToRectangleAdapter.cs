6359e8cf540474ec8b7b62d2278da712c9278e0d
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-11-26 19:04:41 +0100

    Adapter

---
+++ DesignPatterns/DesignPatterns/Adapter/SquareToRectangleAdapter.cs
@@ -0,0 +1,33 @@
+﻿namespace DesignPatterns.Adapter
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public SquareToRectangleAdapter(Square square)
+        {
+            Width = square.Side;
+            Height = square.Side;
+        }
+
+        public int Width { get; }
+        public int Height { get; }
+    }
+}
