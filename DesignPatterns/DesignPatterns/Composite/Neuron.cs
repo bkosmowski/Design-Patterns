80cca075d53309600b8a0d9eb0a47fe65a3fe065
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-11-29 22:12:43 +0100

    Neurons in composite

---
+++ DesignPatterns/DesignPatterns/Composite/Neuron.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization.Formatters;
+
+namespace DesignPatterns.Composite
+{
+    public static class NeuronExtensions
+    {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if(ReferenceEquals(self, other)) return;
+
+            foreach (var from in self)
+            {
+                foreach (var to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }
+            }
+        }
+    }
+
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float Value { get; set; }
+        public List<Neuron> In { get; } = new List<Neuron>();
+        public List<Neuron> Out { get; } = new List<Neuron>();
+
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+}
--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -8,16 +8,14 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            var drawing = new GraphicObject { Name = "My Drawing" };
-            drawing.Children.Add(new GraphicObject.Square { Color = "Red" });
-            drawing.Children.Add(new GraphicObject.Circle { Color = "Yellow" });
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+            var layer1 = new NeuronLayer();
+            var layer2 = new NeuronLayer();
 
-            var group = new GraphicObject();
-            group.Children.Add(new GraphicObject.Circle { Color = "Blue" });
-            group.Children.Add(new GraphicObject.Square { Color = "Blue" });
-            drawing.Children.Add(group);
-
-            Console.WriteLine(drawing);
+            neuron1.ConnectTo(neuron2);
+            neuron1.ConnectTo(layer1);
+            layer1.ConnectTo(layer2);
 
             Console.ReadKey();
         }
