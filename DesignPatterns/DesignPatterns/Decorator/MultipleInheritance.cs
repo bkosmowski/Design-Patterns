11d2faeb4dacf411b9b30e204e302614ced9c487
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-12-01 13:08:14 +0100

    Decorator multiple inheritance

---
+++ DesignPatterns/DesignPatterns/Decorator/MultipleInheritance.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DesignPatterns.Decorator
+{
+    public interface IBird
+    {
+        void Fly();
+
+        int Weight { get; set; }
+    }
+
+    public class Bird : IBird
+    {
+        public void Fly() => Console.WriteLine("I can fly in the sky!");
+        public int Weight { get; set; }
+    }
+
+    public interface ILizard
+    {
+        void Crawl();
+
+        int Weight { get; set; }
+    }
+
+    public class Lizard : ILizard
+    {
+        public void Crawl() => Console.WriteLine("I can crawl in the dirt!");
+        public int Weight { get; set; }
+    }
+
+    public class Dragon : ILizard, IBird //Lizard, Bird
+    {
+        private readonly Lizard _lizard = new Lizard();
+        private readonly Bird _bird = new Bird();
+
+        public void Crawl()
+        {
+            _lizard.Crawl();
+        }
+
+        public void Fly()
+        {
+            _bird.Fly();
+        }
+
+        private int _weight;
+
+        public int Weight
+        {
+            get => _weight;
+            set
+            {
+                _weight = value;
+                _lizard.Weight = value;
+                _bird.Weight = value;
+            }
+        }
+    }
+}
