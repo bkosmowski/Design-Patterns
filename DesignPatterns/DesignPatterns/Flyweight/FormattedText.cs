da47df3f5ca6197db02d2d040947ff0eb3788fd5
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-12-01 21:11:59 +0100

    Formatted text

---
+++ DesignPatterns/DesignPatterns/Flyweight/FormattedText.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Flyweight
+{
+    public class FormattedText
+    {
+        private readonly string _plainText;
+        private readonly bool[] _capitalize; 
+
+        public FormattedText(string plainText)
+        {
+            _plainText = plainText;
+            _capitalize = new bool[plainText.Length];
+        }
+
+        public void Capitalize(int start, int end)
+        {
+            for (var index = start; index < end; index++)
+            {
+                _capitalize[index] = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < _plainText.Length; i++)
+            {
+                var c = _plainText[i];
+                sb.Append(_capitalize[i] ? char.ToUpper(c) : c);
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class BetterFormattedText
+    {
+        private readonly string _plainText;
+
+        private readonly List<TextRange> formatting = new List<TextRange>();
+
+        public BetterFormattedText(string plainText)
+        {
+            _plainText = plainText;
+        }
+
+        public TextRange GetRange(int start, int end)
+        {
+            var range = new TextRange { Start = start, End = end };
+            formatting.Add(range);
+            return range;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < _plainText.Length; i++)
+            {
+                var c = _plainText[i];
+                foreach (var range in formatting)
+                    if (range.Covers(i) && range.Capitalize)
+                        c = char.ToUpperInvariant(c);
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+
+        public class TextRange
+        {
+            public int Start, End;
+            public bool Capitalize, Bold, Italic;
+
+            public bool Covers(int position)
+            {
+                return position >= Start && position <= End;
+            }
+        }
+    }
+}
