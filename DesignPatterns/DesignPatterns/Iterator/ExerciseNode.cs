6d01311b77515791532603a3828a26040ade2aab
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-12-14 22:34:22 +0100

    Node exercise

---
+++ DesignPatterns/DesignPatterns/Iterator/ExerciseNode.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Iterator
+{
+    public class ExerciseNode
+    {
+        public class Node<T>
+        {
+            public T Value;
+            public Node<T> Left, Right;
+            public Node<T> Parent;
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public Node(T value, Node<T> left, Node<T> right)
+            {
+                Value = value;
+                Left = left;
+                Right = right;
+
+                left.Parent = right.Parent = this;
+            }
+
+            public IEnumerable<T> PreOrder
+            {
+                get
+                {
+                    foreach (var root in TraversePreOrder(this))
+                    {
+                        yield return root.Value;
+                    }
+                }
+            }
+
+            private IEnumerable<Node<T>> TraversePreOrder(Node<T> current)
+            {
+                yield return current;
+                if (current.Left != null)
+                {
+                    foreach (var left in TraversePreOrder(current.Left))
+                    {
+                        yield return left;
+                    }
+                }
+
+                if (current.Right != null)
+                {
+                    foreach (var right in TraversePreOrder(current.Right))
+                    {
+                        yield return right;
+                    }
+                }
+            }
+        }
+    }
+}
