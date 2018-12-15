24d9e7f35e08f39bf022ad0cca6a1861765ae0bb
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-12-15 20:26:15 +0100

    Memento exercise

---
+++ DesignPatterns/DesignPatterns/Memento/Exercise.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.Memento
+{
+    public class Exercise
+    {
+        public class Token
+        {
+            public Token(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get; set; }
+        }
+
+        public class Memento
+        {
+            public List<Token> Tokens { get; set; }
+        }
+
+        public class TokenMachine
+        {
+            public List<Token> Tokens = new List<Token>();
+
+            public Memento AddToken(int value)
+            {
+                Tokens.Add(new Token(value));
+                var memento = new Memento { Tokens = Tokens.Select(t => new Token(t.Value)).ToList() };
+                return memento;
+            }
+
+            public Memento AddToken(Token token)
+            {
+                Tokens.Add(token);
+                var memento = new Memento { Tokens = Tokens.Select(t => new Token(t.Value)).ToList() };
+                return memento;
+            }
+
+            public void Revert(Memento m)
+            {
+                Tokens = m.Tokens.ToList();
+            }
+        }
+    }
+}
