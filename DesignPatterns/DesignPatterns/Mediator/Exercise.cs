e7611a48b6932ed256b03e91f3b6271744d71d04
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-12-15 14:40:03 +0100

    Mediator exercise

---
+++ DesignPatterns/DesignPatterns/Mediator/Exercise.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Mediator
+{
+    public class Exercise
+    {
+        public class Participant
+        {
+            private readonly Mediator _mediator;
+
+            public int Value { get; set; }
+
+            public Participant(Mediator mediator)
+            {
+                _mediator = mediator;
+                _mediator.Join(this);
+            }
+
+            public void Say(int n)
+            {
+                _mediator.Broadcast(this, n);
+            }
+        }
+
+        public class Mediator
+        {
+            private readonly List<Participant> _participants = new List<Participant>();
+
+            public void Join(Participant participant)
+            {
+                _participants.Add(participant);
+            }
+
+            public void Broadcast(Participant participant, int i)
+            {
+                foreach (var participant1 in _participants.Where(x => x != participant))
+                {
+                    participant1.Value = i;
+                }
+            }
+        }
+    }
+}
