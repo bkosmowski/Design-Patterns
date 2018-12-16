125cb0b390a0ef1e7471d23be7b56f08f32ec82c
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-12-16 13:47:38 +0100

    Strategy exercise

---
+++ DesignPatterns/DesignPatterns/Strategy/Exercise.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Numerics;
+
+namespace DesignPatterns.Strategy
+{
+    public class Exercise
+    { 
+        public interface IDiscriminantStrategy
+        {
+            double CalculateDiscriminant(double a, double b, double c);
+        }
+
+        public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy
+        {
+            public double CalculateDiscriminant(double a, double b, double c)
+            {
+                return  b * b - 4 * a * c;
+            }
+        }
+
+        public class RealDiscriminantStrategy : IDiscriminantStrategy
+        {
+            public double CalculateDiscriminant(double a, double b, double c)
+            {
+                var result = b * b - 4 * a * c;
+                return result < 0 ? Double.NaN : result;
+            }
+        }
+
+        public class QuadraticEquationSolver
+        {
+            private readonly IDiscriminantStrategy _strategy;
+
+            public QuadraticEquationSolver(IDiscriminantStrategy strategy)
+            {
+                _strategy = strategy;
+            }
+
+            public Tuple<Complex, Complex> Solve(double a, double b, double c)
+            {
+                var discriminant = _strategy.CalculateDiscriminant(a, b, c);
+
+                var sqrt = Complex.Sqrt(discriminant);
+
+                return new Tuple<Complex, Complex>(
+                    (-b + sqrt) / (2 * a),
+                    (-b - sqrt) / (2 * a));
+            }
+        }
+    }
+}
