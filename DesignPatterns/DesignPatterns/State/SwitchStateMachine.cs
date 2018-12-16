edb64a45e627aff64d71a92ff576178a6ec747b7
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-12-16 11:14:43 +0100

    SwitchStateMachine

---
+++ DesignPatterns/DesignPatterns/State/SwitchStateMachine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace DesignPatterns.State
+{
+    public class SwitchStateMachine
+    {
+        public enum State
+        {
+            Locked,
+            Failed,
+            Unlocked
+        }
+        public void Demo()
+        {
+            var code = "1234";
+            var state = State.Locked;
+            var entry = new StringBuilder();
+
+            while (true)
+            {
+                switch (state)
+                {
+                    case State.Locked:
+                        entry.Append(Console.ReadKey().KeyChar);
+
+                        if (entry.ToString() == code)
+                        {
+                            state = State.Unlocked;
+                            break;
+                        }
+
+                        if (!code.StartsWith(entry.ToString()))
+                        {
+                            state = State.Failed;
+                        }
+                        break;
+                    case State.Failed:
+                        Console.CursorLeft = 0;
+                        Console.WriteLine("FAILED");
+                        entry.Clear();
+                        state = State.Locked;
+                        break;
+                    case State.Unlocked:
+                        Console.CursorLeft = 0;
+                        Console.WriteLine("UNLOCKED");
+                        return;
+                }
+            }
+        }
+    }
+    }
+}
