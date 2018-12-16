9137d5a0f2d0c69ec87b0a6188092c3d0440e303
blazej.kosmowski <blazejkosmowski2@gmail.com>
2018-12-16 15:05:58 +0100

    Template method exercise

---
+++ DesignPatterns/DesignPatterns/TemplateMethod/Exercise.cs
@@ -0,0 +1,69 @@
+﻿namespace DesignPatterns.TemplateMethod
+{
+    public class Exercise
+    {
+        public class Creature
+        {
+            public int Attack, Health;
+
+            public Creature(int attack, int health)
+            {
+                Attack = attack;
+                Health = health;
+            }
+        }
+
+        public abstract class CardGame
+        {
+            public Creature[] Creatures;
+
+            protected CardGame(Creature[] creatures)
+            {
+                Creatures = creatures;
+            }
+
+            // returns -1 if no clear winner (both alive or both dead)
+            public int Combat(int creature1, int creature2)
+            {
+                Creature first = Creatures[creature1];
+                Creature second = Creatures[creature2];
+                Hit(first, second);
+                Hit(second, first);
+                bool firstAlive = first.Health > 0;
+                bool secondAlive = second.Health > 0;
+                if (firstAlive == secondAlive) return -1;
+                return firstAlive ? creature1 : creature2;
+            }
+
+            // attacker hits other creature
+            protected abstract void Hit(Creature attacker, Creature other);
+        }
+
+        public class TemporaryCardDamageGame : CardGame
+        {
+            // todo
+            public TemporaryCardDamageGame(Creature[] creatures) : base(creatures)
+            {
+            }
+
+            protected override void Hit(Creature attacker, Creature other)
+            {
+                if(attacker.Attack < other.Health)
+                    return;
+                other.Health -= attacker.Attack;
+            }
+        }
+
+        public class PermanentCardDamage : CardGame
+        {
+            public PermanentCardDamage(Creature[] creatures) : base(creatures)
+            {
+            }
+
+            protected override void Hit(Creature attacker, Creature other)
+            {
+                other.Health -= attacker.Attack;
+            }
+        }
+    }
+}
